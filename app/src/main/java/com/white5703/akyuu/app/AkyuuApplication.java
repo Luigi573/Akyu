3ee077c6118457b457e69b8b61ab9c78aed36090
white5703 <white57030@gmail.com>
2019-01-26 02:17:17 +0800

    chore: remove test code

--- app/src/main/java/com/white5703/akyuu/app/AkyuuApplication.java
+++ app/src/main/java/com/white5703/akyuu/app/AkyuuApplication.java
@@ -2,12 +2,8 @@ package com.white5703.akyuu.app;
 
 import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
-import android.view.Gravity;
 import com.white5703.akyuu.dao.DaoMaster;
 import com.white5703.akyuu.dao.DaoSession;
-import com.white5703.akyuu.manager.DbManager;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AkyuuApplication extends Application {
 
@@ -25,15 +21,15 @@ public class AkyuuApplication extends Application {
 
         initDao();
 
-        DbManager.clearTableNote();
-
-        DbManager.insertDefaultNote();
-        List<String> latexs = new ArrayList<>();
-        latexs.add("$$\\int_a^b f(x)\\mathrm{d}x$$");
-        latexs.add("\\sum_{i=1}^{n} c_{i}");
-
-        DbManager.insertNote("测试", "测试★\n★", "测试", 5, "测试",
-            Gravity.START, 24, Gravity.CENTER, 18, latexs);
+        //DbManager.clearTableNote();
+        //
+        //DbManager.insertDefaultNote();
+        //List<String> latexs = new ArrayList<>();
+        //latexs.add("$$\\int_a^b f(x)\\mathrm{d}x$$");
+        //latexs.add("\\sum_{i=1}^{n} c_{i}");
+        //
+        //DbManager.insertNote("测试", "测试★\n★", "测试", 5, "测试",
+        //    Gravity.START, 24, Gravity.CENTER, 18, latexs);
     }
 
 
